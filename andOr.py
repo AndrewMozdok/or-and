905d048d2bd2bdb2f72c389eb0bf1fbb6caf27a1
AndrewMozdok <57283324+AndrewMozdok@users.noreply.github.com>
2019-11-04 13:39:21 +0300

    Add files via upload

---
+++ andOr.py
@@ -0,0 +1,8 @@
+team = input('Введите любимую футбольную команду: ').upper()
+sport = input('Введите свой любимый вид спорт: ').upper()
+if team == 'REAL MADRID' and sport == 'FOOTBALL':
+    print('Hala Madrid!!!')
+# elif team == 'BARCA':
+#     print('Loshara sosi hyi!!!')
+else:
+    print('Olololo!!!')
